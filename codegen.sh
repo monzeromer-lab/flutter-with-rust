253030bfb39a3a696c0fdabdd5c9b6888b1b739e
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-07-26 17:25:19 +0200

    first commit

---
+++ codegen.sh
@@ -0,0 +1,5 @@
+
+# cargo install flutter_rust_bridge_codegen
+
+flutter_rust_bridge_codegen --rust-input path/to/api.rs +otherfiles-dirs \
+                            --dart-output path/to/bridge_generated.dart
